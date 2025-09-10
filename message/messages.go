f2938794ceb473caea603c59abc0ecfa6218259b
Nicholas Dartora <dartora.nicholas@gmail.com>
2025-09-10 11:14:08 +0200

    Invertita traduzione da inglese --> italiano a italiano --> inglese

--- message/messages.go
+++ message/messages.go
@@ -12,7 +12,7 @@ import (
 // 200
 func Ok(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Task performed successfully"),
+		Message: GetPrinter(c).Sprintf("Operazione eseguita con successo"),
 		Status:  http.StatusOK,
 	}
 }
@@ -22,7 +22,7 @@ func Ok(c *gin.Context) Message {
 // 307
 func ItemFound(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Item found"),
+		Message: GetPrinter(c).Sprintf("Elemento trovato"),
 		Status:  http.StatusTemporaryRedirect,
 	}
 }
@@ -32,14 +32,14 @@ func ItemFound(c *gin.Context) Message {
 // 400
 func BadRequest(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Bad request"),
+		Message: GetPrinter(c).Sprintf("Richiesta non valida"),
 		Status:  http.StatusBadRequest,
 	}
 }
 
 func InvalidUrlParameter(c *gin.Context, parameter string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Missing or invalid required %s field in URL", parameter),
+		Message: GetPrinter(c).Sprintf("Il parametro in URL %s risulta mancante o non valido", parameter),
 		Status:  http.StatusBadRequest,
 	}
 }
@@ -47,28 +47,28 @@ func InvalidUrlParameter(c *gin.Context, parameter string) Message {
 // 403
 func Forbidden(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Forbidden"),
+		Message: GetPrinter(c).Sprintf("Accesso vietato"),
 		Status:  http.StatusForbidden,
 	}
 }
 
 func InsufficientPermissions(c *gin.Context, permissions ...string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Permissions %s are required to access this resource, please contact your administrator", strings.Join(permissions, ",")),
+		Message: GetPrinter(c).Sprintf("I permessi %s sono richiesti per accedere a questa risorsa, per supporto contatta il tuo amministratore", strings.Join(permissions, ",")),
 		Status:  http.StatusForbidden,
 	}
 }
 
 func InsufficientPermissionsHasOne(c *gin.Context, permissions ...string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Permissions %s are required to access this resource, please contact your administrator", strings.Join(permissions, ",")),
+		Message: GetPrinter(c).Sprintf("Almeno uno di questi permessi %s è richiesto per accedere a questa risorsa, per supporto contatta il tuo amministratore", strings.Join(permissions, ",")),
 		Status:  http.StatusForbidden,
 	}
 }
 
 func UnauthorizedRelations(c *gin.Context, relations ...string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("You do not have sufficient permissions to access the following relations: %s", strings.Join(relations, ",")),
+		Message: GetPrinter(c).Sprintf("Non hai autorizzazioni sufficienti per accedere alle seguenti relazioni: %s", strings.Join(relations, ",")),
 		Status:  http.StatusForbidden,
 	}
 }
@@ -76,21 +76,21 @@ func UnauthorizedRelations(c *gin.Context, relations ...string) Message {
 // 404
 func ItemNotFound(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The requested item was not found"),
+		Message: GetPrinter(c).Sprintf("La risorsa richiesta non è stata trovata"),
 		Status:  http.StatusNotFound,
 	}
 }
 
 func FileNotFound(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The requested file was not found"),
+		Message: GetPrinter(c).Sprintf("Il file richiesto non è stato trovato"),
 		Status:  http.StatusNotFound,
 	}
 }
 
 func FolderNotFound(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The requested folder was not found"),
+		Message: GetPrinter(c).Sprintf("La cartella richiesta non è stata trovata"),
 		Status:  http.StatusNotFound,
 	}
 }
@@ -105,21 +105,21 @@ func Conflict(c *gin.Context) Message {
 
 func DeleteFailed(c *gin.Context, blockingRelations []string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Cannot delete the requested resource because it belongs to the following relations.<br>%s", strings.Join(blockingRelations, "<br>")),
+		Message: GetPrinter(c).Sprintf("Non è possibile eliminare la risorsa desiderata perché è utilizzata nelle seguenti relazioni.<br>%s", strings.Join(blockingRelations, "<br>")),
 		Status:  http.StatusConflict,
 	}
 }
 
 func DuplicateUnique(c *gin.Context, table, combination string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The combination %s already exists for %s", combination, table),
+		Message: GetPrinter(c).Sprintf("La combinazione %s esiste già per %s", combination, table),
 		Status:  http.StatusConflict,
 	}
 }
 
 func CannotDeleteSharedResource(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Cannot delete the shared resource as you aren't its owner"),
+		Message: GetPrinter(c).Sprintf("Impossibile eliminare la risorsa condivisa poiché non ne sei il proprietario"),
 		// You cannot delete the shared resource because you are not the owner
 		Status: http.StatusConflict,
 	}
@@ -127,63 +127,63 @@ func CannotDeleteSharedResource(c *gin.Context) Message {
 
 func ConflictingPaginationAndAggregation(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Pagination is not supported with aggregations"),
+		Message: GetPrinter(c).Sprintf("La paginazione non è supportata con le aggregazioni"),
 		Status:  http.StatusConflict,
 	}
 }
 
 func ConflictingOrderByAndDistinct(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The Order field must be specified in the Select field when using Distinct"),
+		Message: GetPrinter(c).Sprintf("Il campo Ordine deve essere specificato nel campo Select quando si usa Distinct"),
 		Status:  http.StatusConflict,
 	}
 }
 
 func ConflictingUsername(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The username is already registered"),
+		Message: GetPrinter(c).Sprintf("Username già registrato"),
 		Status:  http.StatusConflict,
 	}
 }
 
 func ManualPagination(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("To paginate this request you must specify the Order attribute manually via the 'ord' param"),
+		Message: GetPrinter(c).Sprintf("Per paginare la richiesta bisogna specificare l'attributo Order manualmente tramite il parametro in url 'ord'"),
 		Status:  http.StatusConflict,
 	}
 }
 
 func MissingRequiredParameter(c *gin.Context, name, in string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Missing required parameter %s in %s", name, in),
+		Message: GetPrinter(c).Sprintf("Parametro richiesto %s mancante in %s", name, in),
 		Status:  http.StatusConflict,
 	}
 }
 
 func MissingRequiredParameterForQueryField(c *gin.Context, name, field string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Missing required parameter %s in query (eg. &%s=3) for query field %s", name, field),
+		Message: GetPrinter(c).Sprintf("Parametro richiesto %s mancante nella query (es. &%s=3) per campo %s", name, field),
 		Status:  http.StatusConflict,
 	}
 }
 
 func MissingForeignKey(c *gin.Context, key, rel string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Could not find the foreign key %s, required by the relation %s, in its parent object.", key, rel),
+		Message: GetPrinter(c).Sprintf("Impossibile trovare la chiave esterna %s, richiesta dalla relazione %s, nell'oggetto padre.", key, rel),
 		Status:  http.StatusConflict,
 	}
 }
 
 func CannotCreatePrint(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Could not print the warehouseman order print when the order has not been accepted."),
+		Message: GetPrinter(c).Sprintf("Impossibile stampare l'ordine del magazziniere quando l'ordine non è stato accettato."),
 		Status:  http.StatusConflict,
 	}
 }
 
 func MissingBaseResourceSelect(c *gin.Context, baseResource string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Please select at least one element from the base resource %s before accessing nested resources.", baseResource),
+		Message: GetPrinter(c).Sprintf("Seleziona almeno un elemento dalla risorsa base %s prima di accedere alle releazioni nidificate.", baseResource),
 		Status:  http.StatusConflict,
 	}
 }
@@ -191,112 +191,112 @@ func MissingBaseResourceSelect(c *gin.Context, baseResource string) Message {
 // 422
 func Unprocessable(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The submitted request present invalid or incomplete data"),
+		Message: GetPrinter(c).Sprintf("La richiesta inviata contiene dati non validi o incompleti"),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidJSON(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Missing or invalid JSON body"),
+		Message: GetPrinter(c).Sprintf("JSON mancante o non valido"),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidParamsJSON(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The supplied params JSON isn't syntactically valid"),
+		Message: GetPrinter(c).Sprintf("Il JSON dei parametri specificato non è sintatticamente corretto"),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidParamsSyntax(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The request cannot be completed due to invalid params syntax"),
+		Message: GetPrinter(c).Sprintf("La richiesta non è potuta essere completata per via di una sintassi dei parametri errata"),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidField(c *gin.Context, field string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The requested field %s could not be found", field),
+		Message: GetPrinter(c).Sprintf("Il campo %s da voi richiesto non è stato trovato", field),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidFieldValue(c *gin.Context, field, rules string, value interface{}) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The specified value %v for the field %s must respect these constraits %s", value, field, rules),
+		Message: GetPrinter(c).Sprintf("Il valore %v specificato per il campo %s deve rispettare queste condizioni %s", value, field, rules),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidFieldAlias(c *gin.Context, alias, field string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The alias %s specified for the field %s is invalid", alias, field),
+		Message: GetPrinter(c).Sprintf("L'alias %s, specificato per il campo %s, non è valido", alias, field),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidParamOperator(c *gin.Context, operator string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The params operator %s is not supported", operator),
+		Message: GetPrinter(c).Sprintf("L'operatore di parametro %s non è supportato", operator),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidParamType(c *gin.Context, field string, correctType string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The supplied field \"%s\" needs to be of type \"%s\"", field, correctType),
+		Message: GetPrinter(c).Sprintf("Il campo \"%s\" deve essere del tipo \"%s\"", field, correctType),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidRelation(c *gin.Context, table string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Missing or invalid specified relation %s", table),
+		Message: GetPrinter(c).Sprintf("La relazione %s non è stata trovata", table),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidRelations(c *gin.Context, relations ...string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The request cannot be completed due to invalid specified relations: %s", strings.Join(relations, ",")),
+		Message: GetPrinter(c).Sprintf("Non è stato possibile completare la richiesta per via delle seguenti relazioni non valide specificate: %s", strings.Join(relations, ",")),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidOrders(c *gin.Context, orders ...string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The request cannot be completed due to invalid specified order by: %s", strings.Join(orders, ",")),
+		Message: GetPrinter(c).Sprintf("Non è stato possibile completare la richiesta per via dei seguenti ordinamenti non validi specificati: %s", strings.Join(orders, ",")),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func DuplicateStructField(c *gin.Context, field string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Duplicate struct field %s, use an alias to avoid this error (eg. field AS alias)", field),
+		Message: GetPrinter(c).Sprintf("Campo %s duplicato nella struct, usare un alias per evitare questo errore (es. campo AS alias)", field),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func InvalidFieldRequired(c *gin.Context, name string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The %s property is required", name),
+		Message: GetPrinter(c).Sprintf("La proprietà %s è obbligatoria", name),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func RowError(c *gin.Context, row int, message string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("Row %d:%s", row, message),
+		Message: GetPrinter(c).Sprintf("Riga %d:%s", row, message),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
 
 func DisplayNameNotSupported(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("This resource doesn't support DISPLAY_NAME"),
+		Message: GetPrinter(c).Sprintf("Questa risorsa non supporta DISPLAY_NAME"),
 		Status:  http.StatusUnprocessableEntity,
 	}
 }
@@ -305,21 +305,21 @@ func DisplayNameNotSupported(c *gin.Context) Message {
 
 func InternalServerError(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("An error has occurred"),
+		Message: GetPrinter(c).Sprintf("Si è verificato un errore"),
 		Status:  http.StatusInternalServerError,
 	}
 }
 
 func ExpectedSlice(c *gin.Context) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The supplied parameter isn't of type *[]models.*"),
+		Message: GetPrinter(c).Sprintf("Il parametro specificato non è del tipo *[]models.*"),
 		Status:  http.StatusInternalServerError,
 	}
 }
 
 func UnsupportedParamType(c *gin.Context, parameter string) Message {
 	return &Msg{
-		Message: GetPrinter(c).Sprintf("The supplied parameter %s isn't supported yet. This is an error", parameter),
+		Message: GetPrinter(c).Sprintf("Il parametro specificato %s non è ancora supportato.", parameter),
 		Status:  http.StatusInternalServerError,
 	}
 }
