c553f918f3302853c4987d5d43860e0224888338
Davide Cesare <davide.cesare@datosystem.it>
2026-09-01 14:22:08 +0200

    Possibile fix salvataggio decimali quando non ci devono essere

--- datatypes/roundedFloat.go
+++ datatypes/roundedFloat.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-type RoundedFloat float32
+type RoundedFloat float64
 
 func (f RoundedFloat) Value() (driver.Value, error) {
 	return math.Round(float64(f*10000)) / 10000, nil
